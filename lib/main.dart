51294a142619ae54c911e07320e3c8d1267de326
Manvi1718 <manvi073btcsai22@igdtuw.ac.in>
2025-03-21 16:21:51 +0530

    set up firebase firestore and now connected it to my application

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:metafit/firebase_options.dart';
 import 'package:metafit/pages/initial.dart';
 import 'package:metafit/provider/meal_plan_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => MealPlanProvider(),
